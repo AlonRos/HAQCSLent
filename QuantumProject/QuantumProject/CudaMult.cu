781103eac01dcb9eaae70fb0d9df660b49e08a54
AlonRos <alon.rosenperl@gmail.com>
2024-04-23 19:50:16 +0300

    Trying complex in GPU to check if the performance is better

--- QuantumProject/QuantumProject/CudaMult.cu
+++ QuantumProject/QuantumProject/CudaMult.cu
@@ -6,14 +6,14 @@ typedef struct {
 	int width;
 	int height;
 	int stride;
-	double* elements;
+	cuDoubleComplex* elements;
 
 } GpuMatrix;
 
-__device__ double GetElement(GpuMatrix& A, int row, int col) {
+__device__ cuDoubleComplex GetElement(GpuMatrix& A, int row, int col) {
 	return A.elements[row * A.stride + col];
 }
-__device__ void SetElement(GpuMatrix& A, int row, int col, double value) {
+__device__ void SetElement(GpuMatrix& A, int row, int col, cuDoubleComplex value) {
 	A.elements[row * A.stride + col] = value;
 }
 
@@ -33,7 +33,7 @@ __global__ void MatMulKernel(GpuMatrix A, GpuMatrix B, GpuMatrix C, int blockHei
 
 	GpuMatrix subC = GetSubMatrix(C, blockRow, blockCol, blockHeightA, blockWidthB);
 
-	double Cvalue = 0;
+	cuDoubleComplex Cvalue = make_cuDoubleComplex(0, 0);
 
 	int row = threadIdx.y;
 	int col = threadIdx.x;
@@ -43,8 +43,8 @@ __global__ void MatMulKernel(GpuMatrix A, GpuMatrix B, GpuMatrix C, int blockHei
 
 		GpuMatrix subB = GetSubMatrix(B, i, blockCol, blockWidthAHeightB, blockWidthB);
 
-		__shared__ double As[MAX_BLOCK_SIZE][MAX_BLOCK_SIZE];
-		__shared__ double Bs[MAX_BLOCK_SIZE][MAX_BLOCK_SIZE];
+		__shared__ cuDoubleComplex As[MAX_BLOCK_SIZE][MAX_BLOCK_SIZE];
+		__shared__ cuDoubleComplex Bs[MAX_BLOCK_SIZE][MAX_BLOCK_SIZE];
 
 		if (row < subA.height && col < subA.width) {
 			As[row][col] = GetElement(subA, row, col);
@@ -58,7 +58,7 @@ __global__ void MatMulKernel(GpuMatrix A, GpuMatrix B, GpuMatrix C, int blockHei
 		
 		if (row < subC.height && col < subC.width) {
 			for (int j = 0; j < blockWidthAHeightB; ++j)
-				Cvalue += As[row][j] * Bs[j][col];
+				Cvalue = cuCadd(Cvalue, cuCmul(As[row][j], Bs[j][col]));
 		}
 
 		__syncthreads();
@@ -70,26 +70,26 @@ __global__ void MatMulKernel(GpuMatrix A, GpuMatrix B, GpuMatrix C, int blockHei
 }
 
 __host__
-double* gpuMultDouble(double* A, int Am, int An, double* B, int Bn) {
+cuDoubleComplex* gpuMultComplex(cuDoubleComplex* A, int Am, int An, cuDoubleComplex* B, int Bn) {
     GpuMatrix dev_A, dev_B, dev_res;
     int Bm = An, resm = Am, resn = Bn;
 
     dev_A.width = dev_A.stride = An;
     dev_A.height = Am;
-    size_t size = dev_A.width * dev_A.height * sizeof(double);
+    size_t size = dev_A.width * dev_A.height * sizeof(cuDoubleComplex);
     cudaMalloc(&dev_A.elements, size);
     cudaMemcpy(dev_A.elements, A, size, cudaMemcpyHostToDevice);
 
     dev_B.width = dev_B.stride = Bn;
     dev_B.height = Bm;
-    size = dev_B.width * dev_B.height * sizeof(double);
+    size = dev_B.width * dev_B.height * sizeof(cuDoubleComplex);
     cudaMalloc(&dev_B.elements, size);
     cudaMemcpy(dev_B.elements, B, size, cudaMemcpyHostToDevice);
 
 
     dev_res.width = dev_res.stride = resn;
     dev_res.height = resm;
-    size = dev_res.width * dev_res.height * sizeof(double);
+    size = dev_res.width * dev_res.height * sizeof(cuDoubleComplex);
     cudaMalloc(&dev_res.elements, size);
 
 
@@ -103,7 +103,7 @@ double* gpuMultDouble(double* A, int Am, int An, double* B, int Bn) {
 
     MatMulKernel << <dimGrid, dimBlock >> > (dev_A, dev_B, dev_res, blockHeightA, blockWidthAHeightB, blockWidthB);
 
-    double* res = (double*)malloc(size);
+    cuDoubleComplex* res = (cuDoubleComplex*)malloc(size);
 
     cudaMemcpy(res, dev_res.elements, size, cudaMemcpyDeviceToHost);
 
@@ -116,40 +116,33 @@ double* gpuMultDouble(double* A, int Am, int An, double* B, int Bn) {
 
 __host__
 void Matrix2::gpuMultIn(Matrix2& A, Matrix2& B, Matrix2& res) {
-	size_t size = A.m * A.n * sizeof(double);
+	size_t size = A.m * A.n * sizeof(cuDoubleComplex);
 
-	double* realA = (double*) malloc(size);
-    double* imagA = (double*) malloc(size);
+	cuDoubleComplex* Alement = (cuDoubleComplex*) malloc(size);
 
 
 	for (int i = 0; i < A.m; ++i) {
 		for (int j = 0; j < A.n; ++j) {
-            realA[j + i * A.n] = A.entry(i, j).real();
-            imagA[j + i * A.n] = A.entry(i, j).imag();
+			Alement[j + i * A.n] = complexToCudaComplex(A.entry(i, j));
+            //imagA[j + i * A.n] = A.entry(i, j).imag();
 		}
 	}
 
-    size = B.m * B.n * sizeof(double);
-    double* realB = (double*)malloc(size);
-    double* imagB = (double*)malloc(size);
+    size = B.m * B.n * sizeof(cuDoubleComplex);
+    cuDoubleComplex* Blements = (cuDoubleComplex*)malloc(size);
 
 	
     for (int i = 0; i < B.m; ++i) {
         for (int j = 0; j < B.n; ++j) {
-            realB[j + i * B.n] = B.entry(i, j).real();
-            imagB[j + i * B.n] = B.entry(i, j).imag();
+			Blements[j + i * B.n] = complexToCudaComplex(B.entry(i, j));
         }
     }
 
-    double* realArealB = gpuMultDouble(realA, A.m, A.n, realB, B.n);
-    double* imagAimagB = gpuMultDouble(imagA, A.m, A.n, imagB, B.n);
-
-    double* realAimagB = gpuMultDouble(realA, A.m, A.n, imagB, B.n);
-    double* imagArealB = gpuMultDouble(imagA, A.m, A.n, realB, B.n);
+    cuDoubleComplex* reslement = gpuMultComplex(Alement, A.m, A.n, Blements, B.n);
     
 	for (int i = 0; i < A.m; ++i) {
 		for (int j = 0; j < B.n; ++j) {
-            res.entry(i, j) = complex_t(realArealB[j + i * B.n] - imagAimagB[j + i * B.n], realAimagB[j + i * B.n] + imagArealB[j + i * B.n]);
+            res.entry(i, j) = cudaComplexToComplex(reslement[j + i * B.n]);
 		}
 	}
 }
