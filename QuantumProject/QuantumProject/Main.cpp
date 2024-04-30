39890ede8239da6f5bd901402c797147c8e47a5d
AlonRos <alon.rosenperl@gmail.com>
2024-04-30 23:51:59 +0300

    Deutsch algorithm in main

--- QuantumProject/QuantumProject/Main.cpp
+++ QuantumProject/QuantumProject/Main.cpp
@@ -17,11 +17,45 @@
 using namespace std;
 
 int main() {
-	int num = 3;
-	int length = 13;
+	int n = 5;
 
-	Quregister reg(length, num);
+	Quregister q(n + 1, 1);
 
-	Quregister::QFT(reg).getCoords();
+	int coordsLength = q.getCoordsLength(), regLength = n + 1;
+
+	q.applyGateOnQubits(hadamard, 0, regLength);
+
+	Matrix2& Uf = *new Matrix2(coordsLength, coordsLength);
+
+	int* f = new int[1 << n];
+
+	for (int i = 0; i < coordsLength / 8; ++i) {
+		f[i] = 1;
+	}
+
+	for (int i = coordsLength / 8; i < coordsLength / 4; ++i) {
+		f[i] = 0;
+
+	}
+
+	for (int i = coordsLength / 4; i < 3 * coordsLength / 8; ++i) {
+		f[i] = 0;
+
+	}
+
+	for (int i = 3 * coordsLength / 8; i < coordsLength / 2; ++i) {
+		f[i] = 1;
+	}
+
+	for (int i = 0; i < coordsLength / 2; ++i) {
+		Uf.entry(2 * i, (i << 1) | f[i]) = 1;
+		Uf.entry(2 * i + 1, (i << 1) | (1 - f[i])) = 1;
+	}
+
+	q.applyGate(Uf);
+
+	q.applyGateOnQubits(hadamard, 1, regLength);
+
+	cout << q.regMeasureComputational(1, regLength);
 
 }
